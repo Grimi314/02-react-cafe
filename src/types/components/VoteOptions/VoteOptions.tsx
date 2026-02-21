d3285f93ad2ee0490cb3001a95bb332fbba91260
Maryan Serhii <sergijmara210@gmail.com>
2026-02-21 02:12:09 +0200

    add modern-normalize

---
+++ src/types/components/VoteOptions/VoteOptions.tsx
@@ -0,0 +1,32 @@
+import css from "./VoteOptions.module.css";
+import type { Votes } from "../../types/votes";
+interface VoteOptionsProps {
+  onVote: (type: keyof Votes) => void;
+  onReset: () => void;
+  canReset: boolean;
+}
+
+export default function VoteOptions({
+  onReset,
+  onVote,
+  canReset,
+}: VoteOptionsProps) {
+  return (
+    <div className={css.container}>
+      <button className={css.button} onClick={() => onVote("good")}>
+        Good
+      </button>
+      <button className={css.button} onClick={() => onVote("neutral")}>
+        Neutral
+      </button>
+      <button className={css.button} onClick={() => onVote("bad")}>
+        Bad
+      </button>
+      {canReset && (
+        <button onClick={onReset} className={`${css.button} ${css.reset}`}>
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
